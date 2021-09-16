f1b2d1d52a14251fd78f28cbf31389a0cf6a81dd
Bruno Martins <bruno.martins217@gmail.com>
2021-09-16 17:30:43 -0300

    listando repositorios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export default function RepositoryItem({repository}) {
     <li>
       <strong>{repository?.name ?? 'Default'}</strong>
       <p>{repository?.description}</p>
-      <a href={repository?.link}>Acessar Repositório</a>
+      <a href={repository?.html_url}>Acessar Repositório</a>
     </li>
   );
 }
